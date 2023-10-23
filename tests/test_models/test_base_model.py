552553b799b3d14ebe671b6ee52e567087f08f23
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-23 19:36:12 -0300

    jhh

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,10 +2,10 @@ import unittest
 from models.base_model import BaseModel
 
 class Test(unittest.TestCase):
-    """sdsdsd"""
+    """sdsdsd  sdsd s dasd sdsad"""
 
     def test_base_model(self):
-        """bbjbnb"""
+        """bbjbnb sadsd asddas """
         base = BaseModel()
         self.assertEqual(base.__str__(), f"[{type(base).__name__}] ({base.id}) {base.__dict__}")
 
