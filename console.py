2c026295ff0c0933474706410b04a8df015cf560
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-24 13:54:47 -0300

    hff

--- console.py
+++ console.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""sddfsd dfdf df d f"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class command console"""
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """quit command"""
+        return True
+
+    def do_EOF(self,arg):
+        """ctr + d"""
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
