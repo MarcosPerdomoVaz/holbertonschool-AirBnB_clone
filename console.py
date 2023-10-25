05d61ae28a9844f3e23d51b6d57b7e6a4b81314a
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-25 13:46:14 -0300

    r

--- console.py
+++ console.py
@@ -2,19 +2,50 @@
 """sddfsd dfdf df d f"""
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """class command console"""
     prompt = "(hbnb)"
 
+    class_names = ["BaseModel"]
+
     def do_quit(self, arg):
         """quit command"""
         return True
 
-    def do_EOF(self,arg):
+    def do_EOF(self, arg):
         """ctr + d"""
         return True
+
+    def do_create(self, arg):
+        """sd  f df df  df """
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg not in self.class_names:
+            print("** class doesn't exist **")
+        else:
+            cr_inst = BaseModel()
+            cr_inst.save()
+            print(cr_inst.id)
+
+    def do_show(self, arg):
+        """sdsdsd s d sdsa dsd """
+        arg_list = arg.split(" ")
+        if len(arg_list) < 1:
+            print("** class name missing **")
+        elif arg_list[0] not in self.class_names:
+            print("** class doesn't exist **")
+        elif len(arg_list) < 2:
+            print("** instnace id missing **")
+        else:
+            stored_objs = storage.all()
+            if f"{arg_list[0]}.{arg_list[1]}" in stored_objs.keys():
+                print(f"{stored_objs[f'{arg_list[0]}.{arg_list[1]}']}")
+            else:
+                print("** no instance found **")
 
     def emptyline(self):
         pass
