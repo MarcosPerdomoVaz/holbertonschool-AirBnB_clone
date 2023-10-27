deb9388b06040ba0a856273bb2c6760bfb23e5fe
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-27 20:28:57 -0300

    1/2 test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,7 +7,11 @@ from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class."""
+    fs = FileStorage()
     def testFilePath(self):
-        self.assertIsNotNone(FileStorage.__file_path)
+        self.assertIsNotNone(fs._FileStorage__file_path)
     def testObjects(self):
-        self.assertIsNotNone(FileStorage.__objects)
+        self.assertIsNotNone(fs._FileStorage__objects)
+
+if __name__ == '__main__':
+    unittest.main()
